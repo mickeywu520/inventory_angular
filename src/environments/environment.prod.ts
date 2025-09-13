9dace18788e90cb3f0a01df029746c3860f0d43b
mickey <mickeywu520@gmail.com>
2025-09-13 18:26:44 +0800

    add customer name as defaut in batch-modificaion features & update version to v01.0002

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   //apiUrl: 'http://192.168.1.101:5050/api'
   apiUrl:'https://mickeywu520-inventory-fastapi.hf.space/api',
   version: 'v01',
-  subversion: '0001'
+  subversion: '0002'
 };
